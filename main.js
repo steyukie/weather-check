531ea3da5ddb39f1013104236c417317995ad397
steyukie <stefanyukie@gmail.com>
2024-11-29 00:04:23 -0300

    teste

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const apiKey = "23d9a0f4363a1ae0945c7a3cc4dc6d79";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=brazil";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city){
+    try {const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+
 }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+    
+})
+
+checkWeather(city);
